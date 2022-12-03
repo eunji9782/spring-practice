815408d451feecbdde064ff11aa0d589f9a5c41d
DESKTOP-CH3HBB6\user <dmswl9782@gmail.com>
2022-12-03 21:49:04 +0900

    21강 순수 JDBC

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,11 +1,13 @@
 package hello.hellospring.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import hello.hellospring.domain.Member;
@@ -13,12 +15,18 @@ import hello.hellospring.repository.MemoryMemberRepository;
 
 public class MemberServiceTest {
 	
-	MemberService memberService = new MemberService();
-	MemoryMemberRepository repository = new MemoryMemberRepository();
+	MemberService memberService;
+	MemoryMemberRepository memberRepository;
+	
+	@BeforeEach
+	public void beforeEach() {
+		memberRepository = new MemoryMemberRepository();
+		memberService = new MemberService(memberRepository);
+	}
 	
 	@AfterEach
 	public void clear() {
-		repository.clearStore();
+		memberRepository.clearStore();
 	}
 	
 	@Test
@@ -26,7 +34,25 @@ public class MemberServiceTest {
 		Member member = new Member();
 		member.setName("spring");
 		Long result = memberService.join(member);
-		assertThat(result).isEqualTo(1); 
+		
+		Member findMember = memberService.findOne(result).get();
+		assertThat(member.getName()).isEqualTo(findMember.getName()); 
+	}
+	
+	//중복회원 예외 확인
+	@Test
+	public void joinDup() {
+		Member member1 = new Member();
+		member1.setName("spring");
+		
+		Member member2 = new Member();
+		member2.setName("spring");
+		
+		memberService.join(member1);
+		IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+	
+		assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+	
 	}
 	
 	@Test
@@ -47,14 +73,14 @@ public class MemberServiceTest {
 	public void findOne() {
 		Member member = new Member();
 		member.setName("spring");
-		memberService.join(member);
+		Long memberId = memberService.join(member);
 		
 		Member member2 = new Member();
 		member2.setName("spring2");
 		memberService.join(member2);
 		
-		Optional<Member> result = memberService.findOne(1L);
-		assertThat(member).isEqualTo(result);	
+		Member findMember = memberService.findOne(memberId).get();
+		assertThat(member).isEqualTo(findMember);	
 	}
 
 }
